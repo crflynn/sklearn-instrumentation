85b536e0bd690f711138cd288abe7896080fd542
Flynn <crflynn@users.noreply.github.com>
2020-12-02 02:48:48 -0500

    add cprofiler (#18)

---
+++ examples/cprofiler.py
@@ -0,0 +1,44 @@
+import os
+
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import FeatureUnion
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+from sklearn_instrumentation import SklearnInstrumentor
+from sklearn_instrumentation.instruments.cprofile import CProfiler
+
+ss = StandardScaler()
+pca = PCA(n_components=3)
+rf = RandomForestClassifier()
+classification_model = Pipeline(
+    steps=[
+        (
+            "fu",
+            FeatureUnion(
+                transformer_list=[
+                    ("ss", ss),
+                    ("pca", pca),
+                ],
+            ),
+        ),
+        ("rf", rf),
+    ],
+)
+X, y = load_iris(return_X_y=True)
+
+if __name__ == "__main__":
+    profiler = CProfiler()
+    instrumentor = SklearnInstrumentor(
+        instrument=profiler, instrument_kwargs={"out_dir": os.getcwd() + "/../.ignore"}
+    )
+
+    instrumentor.instrument_estimator(
+        classification_model,
+    )
+
+    classification_model.fit(X, y)
+
+    classification_model.predict(X)
